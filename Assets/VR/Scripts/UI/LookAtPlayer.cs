674b4297e42649f06d7b97e51df19a299e895b17
rumster89833439469 <rumster_rt@mail.ru>
2017-04-29 01:41:30 +0700

    Added menu button images

---
+++ Assets/VR/Scripts/UI/LookAtPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour {
+
+    protected Transform m_Player;
+
+    void Awake()
+    {
+        m_Player = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        if (m_Player == null)
+            return;
+
+        transform.LookAt(m_Player);
+    }
+}
--- Assets/VR/Scripts/UI/MenuButton.cs
+++ Assets/VR/Scripts/UI/MenuButton.cs
@@ -3,9 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(MeshRenderer))]
 public class MenuButton : CameraEyeRaycastTarget {
 
     public string LoadSceneName = "";
+
+    public float updateImageTime = 0.5f;
+
+    [SerializeField]
+    protected List<Texture> m_ImageStack;
+
+    protected Material material;
+
+    protected void Start()
+    {
+        if (GetComponent<MeshRenderer>() != null)
+        {
+            material = GetComponent<MeshRenderer>().material;
+
+            currentIndex = Random.Range(0, m_ImageStack.Count - 1);
+
+            InvokeRepeating("UpdateImage", 0, updateImageTime);
+        }
+    }
+
+    int currentIndex = 0;
+
+    bool isInc = true;
+
+    protected void UpdateImage()
+    {
+
+        if (m_ImageStack == null || m_ImageStack.Count == 0)
+            return;
+
+        material.mainTexture = m_ImageStack[currentIndex];
+
+        currentIndex += isInc ? 1 : -1;
+
+        if (currentIndex + 1 >= m_ImageStack.Count || currentIndex - 1 < 0)
+        {
+            isInc = !isInc;
+            currentIndex = isInc ? 0 : m_ImageStack.Count -1 ;
+        }
+    }
 
     protected override void OnActivated()
     {
