5e6b95789ee01621b67a789f4cea3935fdfd7049
rumster89833439469 <rumster_rt@mail.ru>
2017-04-30 00:07:21 +0700

    Added four 360 scene, player ui

--- Assets/GoogleVR/Demos/Scripts/VideoDemo/AutoPlayVideo.cs
+++ Assets/GoogleVR/Demos/Scripts/VideoDemo/AutoPlayVideo.cs
@@ -16,52 +16,61 @@
 // </copyright>
 
 namespace GVRSample {
-  using UnityEngine;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
 
-  /// <summary>
-  /// Auto play video.
-  /// </summary>
-  /// <remarks>This script exposes a delay value in seconds to start playing the TexturePlayer
-  /// component on the same object.
-  /// </remarks>
+    /// <summary>
+    /// Auto play video.
+    /// </summary>
+    /// <remarks>This script exposes a delay value in seconds to start playing the TexturePlayer
+    /// component on the same object.
+    /// </remarks>
 
-  [RequireComponent(typeof(GvrVideoPlayerTexture))]
-  public class AutoPlayVideo : MonoBehaviour {
-    private bool done;
-    private float t;
-    private GvrVideoPlayerTexture player;
+    [RequireComponent(typeof(GvrVideoPlayerTexture))]
+    public class AutoPlayVideo : MonoBehaviour {
 
-    public float delay = 2f;
-    public bool loop = false;
+        public string ExitSceneName = "";
 
-    void Start() {
-      t = 0;
-      done = false;
-      player = GetComponent<GvrVideoPlayerTexture>();
-      if (player != null) {
-        player.Init();
-      }
+        private bool done;
+        private float t;
+        private GvrVideoPlayerTexture player;
+
+        public float delay = 2f;
+        public bool loop = false;
+
+        void Start() {
+            t = 0;
+            done = false;
+            player = GetComponent<GvrVideoPlayerTexture>();
+            if (player != null) {
+                player.Init();
+            }
+        }
+
+        void Update() {
+            if (player == null) {
+                return;
+            } else if (player.PlayerState == GvrVideoPlayerTexture.VideoPlayerState.Ended && done && loop) {
+                player.Pause();
+                player.CurrentPosition = 0;
+                done = false;
+                t = 0f;
+                return;
+            }
+            if (done) {
+              return;
+            }
+
+            if(player.PlayerState == GvrVideoPlayerTexture.VideoPlayerState.Ended)
+            {
+                SceneManager.LoadScene(ExitSceneName);
+            }
+
+            t += Time.deltaTime;
+            if (t >= delay && player != null) {
+                player.Play();
+                done = true;
+            }
+        }
     }
-
-    void Update() {
-      if (player == null) {
-        return;
-      } else if (player.PlayerState == GvrVideoPlayerTexture.VideoPlayerState.Ended && done && loop) {
-        player.Pause();
-        player.CurrentPosition = 0;
-        done = false;
-        t = 0f;
-        return;
-      }
-      if (done) {
-        return;
-      }
-
-      t += Time.deltaTime;
-      if (t >= delay && player != null) {
-        player.Play();
-        done = true;
-      }
-    }
-  }
 }
